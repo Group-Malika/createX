2db0b298779ec1a8646446c1e462aba9a1f7282b
MalikaMarselovna <148754005+MalikaMarselovna@users.noreply.github.com>
2024-02-24 22:49:24 +0600

    add a smooth scrolling for every "a"

--- js/core.js
+++ js/core.js
@@ -25,6 +25,22 @@ $(document).ready(function() {
     });
 });
 
+
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute('href');
+
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
+
+
 //progress bar//   
 
 
